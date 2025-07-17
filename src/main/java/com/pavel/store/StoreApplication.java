56d504e62ec912d70916f0ad62116a56a556cf71
pavel <ppetrashov.dev@gmail.com>
2025-07-16 22:32:23 -0400

    Configure PostgreSQL datasource and fix application entry point class name.

--- src/main/java/com/pavel/store/StoreApplication.java
+++ src/main/java/com/pavel/store/StoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HighloadStoreApplication.class, args);
+        SpringApplication.run(StoreApplication.class, args);
     }
 
 }
