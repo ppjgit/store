5b77f6f6e217a1b0859590c733df8dfd8ab6ff26
pavel <ppetrashov.dev@gmail.com>
2025-07-16 22:26:47 -0400

    Initialize Spring Boot project with Gradle build system, basic configuration, and project structure.

---
+++ src/main/java/com/pavel/store/StoreApplication.java
@@ -0,0 +1,13 @@
+package com.pavel.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighloadStoreApplication.class, args);
+    }
+
+}
